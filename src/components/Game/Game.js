dc86efd069c56ecb2fd61ea7f2fce99fec6911b7
Yusuf Alp <alpyusuf007@gmail.com>
2021-01-21 09:30:47 -0800

    Add Game component to control the state of the game

---
+++ src/components/Game/Game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Board from '../Board/Board';
+
+const Game = () => {
+  return (
+    <div>
+      <Board
+        rows={4}
+      />
+    </div>
+  );
+}
+
+export default Game;
