71e9d517242231a77883fe3bc02cf6162c67c1a9
Yusuf Alp <alpyusuf007@gmail.com>
2021-01-21 13:13:21 -0800

    Update class names

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -4,7 +4,9 @@ import './Grid.css';
 const Grid = (props) => {
   return (
     <>
-      <div className="grid" onClick={() => props.handleGridClick(props.index)}>
+      <div
+        className={`grid ${props.rows >= 10 ? "small-grid" : ""}`}
+        onClick={() => props.handleGridClick(props.index)}>
         {props.value}
       </div>
       {props.index % props.rows === props.rows - 1 ? <br /> : ''}
