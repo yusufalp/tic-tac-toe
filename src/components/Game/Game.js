8f99f673fbb7f596837ba3d2f346e0a75d3f20d9
Yusuf Alp <alpyusuf007@gmail.com>
2021-01-21 11:56:12 -0800

    Update the button click to start the game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Board from '../Board/Board';
 import Dropdown from '../Dropdown/Dropdown';
-import Button from '../../utils/Button/Button';
+import Button from '../Button/Button';
 
 const boardSizes = [];
 for (let i = 3; i <= 20; i++) {
@@ -12,29 +12,25 @@ const Game = () => {
   const [rows, setRows] = useState(3);
   const [didStart, setDidStart] = useState(false);
 
-  const handleStart = () => {
-    setDidStart(true);
-  }
-
   const handleReset = () => {
     setRows(3);
     setDidStart(false);
   }
 
   const renderedGame = didStart
-    ? (<>
+    ? (<React.Fragment>
       <Button handleClick={handleReset}>Reset</Button>
       <Board
         rows={rows}
       />
-    </>)
-    : (<>
-      <Button handleClick={handleStart}>Start the Game</Button>
+    </React.Fragment>)
+    : (<React.Fragment>
+      <Button handleClick={() => setDidStart(true)}>Start the Game</Button>
       <Dropdown
         items={boardSizes}
         handleSelect={(e) => setRows(Number(e.target.value))}
       />
-    </>)
+    </React.Fragment>)
 
   return (
     <div>
