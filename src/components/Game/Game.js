149c5fdff41f1c46bf1713485ec2edbb8bef0c56
Yusuf Alp <alpyusuf007@gmail.com>
2021-01-21 10:17:10 -0800

    Add state control for board size selection and game start

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from '../Board/Board';
+import Dropdown from '../Dropdown/Dropdown';
+import Button from '../../utils/Button/Button';
+
+const boardSizes = [];
+for (let i = 3; i <= 20; i++) {
+  boardSizes.push(i);
+}
 
 const Game = () => {
+  const [rows, setRows] = useState(3);
+  const [didStart, setDidStart] = useState(false);
+
+  const handleStart = () => {
+    setDidStart(true);
+  }
+
+  const handleReset = () => {
+    setRows(3);
+    setDidStart(false);
+  }
+
+  const renderedGame = didStart
+    ? (<>
+      <Button handleClick={handleReset}>Reset</Button>
+      <Board
+        rows={rows}
+      />
+    </>)
+    : (<>
+      <Button handleClick={handleStart}>Start the Game</Button>
+      <Dropdown
+        items={boardSizes}
+        handleSelect={(e) => setRows(Number(e.target.value))}
+      />
+    </>)
+
   return (
     <div>
-      <Board
-        rows={4}
-      />
+      {renderedGame}
     </div>
   );
 }
