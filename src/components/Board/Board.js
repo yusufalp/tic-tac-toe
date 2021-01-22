35504c0909a44f46af295ab006495243444b04c1
Yusuf Alp <alpyusuf007@gmail.com>
2021-01-22 07:36:04 -0800

    Update the board with reset button and maintain state after reload

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,65 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '../Grid/Grid';
+import Dropdown from '../Dropdown/Dropdown';
+import Button from '../Button/Button';
 import './Board.css';
+import { announceWinner } from '../AnnounceWinner/AnnounceWinner';
 
-const Board = (props) => {
-  const [isXsTurn, setIsXsTurn] = useState(true);
-  const [allGrids, setAllGrids] = useState(Array(props.rows * props.rows).fill(null));
+const boardSizes = [];
+for (let i = 3; i <= 20; i++) {
+  boardSizes.push(i);
+}
 
-  const announceWinner = () => {
-    const result = [];
+const Board = () => {
+  const [rows, setRows] = useState(Number(sessionStorage.getItem("initial-rows-selected")) || 3);
+  const [isXsTurn, setIsXsTurn] = useState(sessionStorage.getItem("isXsTurn") ? sessionStorage.getItem("isXsTurn") === "true" : true);
+  const [allGrids, setAllGrids] = useState(JSON.parse(sessionStorage.getItem("played-grids")) || Array(rows * rows).fill(null));
 
-    let linesToCheck = [];
-    let index = 0;
+  useEffect(() => {
+    sessionStorage.setItem("initial-rows-selected", rows.toString());
+    sessionStorage.setItem("played-grids", JSON.stringify(allGrids));
+    sessionStorage.setItem("isXsTurn", isXsTurn.toString());
+  })
 
-    for (let i = 0; i < props.rows; i++) {
-      linesToCheck = [];
-      for (let j = 0; j < props.rows; j++) {
-        linesToCheck.push(allGrids[j + index]);
-      }
-      result.push(linesToCheck);
-      index += props.rows;
-    }
-
-    for (let i = 0; i < props.rows; i++) {
-      index = i;
-      linesToCheck = [];
-      for (let j = 0; j < props.rows; j++) {
-        linesToCheck.push(allGrids[index]);
-        index += props.rows;
-      }
-      result.push(linesToCheck);
-    }
-
-    linesToCheck = [];
-    index = 0;
-    for (let i = 0; i < props.rows; i++) {
-      linesToCheck.push(allGrids[i + index]);
-      index += props.rows;
-    }
-    result.push(linesToCheck);
-
-    linesToCheck = [];
-    index = props.rows - 1;
-    let start = allGrids.length - 1;
-    for (let i = 0; i < props.rows; i++) {
-      linesToCheck.push(allGrids[start - index]);
-      index += props.rows - 1;
-    }
-    result.push(linesToCheck);
-
-    const isX = currentValue => currentValue === "X";
-    const isO = currentValue => currentValue === "O";
-    for (let i = 0; i < result.length; i++) {
-      let isAllX = result[i].every(isX);
-      let isAllO = result[i].every(isO)
-      if (isAllX || isAllO) {
-        return `Winner is ${isAllX ? "X" : "O"}`;
-      }
-    }
-  }
-
-  let winner = announceWinner();
+  let winner = announceWinner(rows, allGrids);
   let status = "";
 
   if (!winner) {
@@ -71,6 +33,13 @@ const Board = (props) => {
     }
   } else {
     status = winner;
+  }
+
+  let winnerClassName = "";
+  if (winner === "Winner is X") {
+    winnerClassName = "red-win";
+  } else if (winner === "Winner is O") {
+    winnerClassName = "green-win";
   }
 
   const handleGridClick = (index) => {
@@ -85,21 +54,34 @@ const Board = (props) => {
     setIsXsTurn(!isXsTurn);
   }
 
+  const handleReset = (e) => {
+    let selectedValue = e.target.value;
+    let selectedRows = Number(selectedValue) === 0 ? rows : Number(selectedValue);
+    setIsXsTurn(true);
+    setRows(selectedRows)
+    setAllGrids(Array(selectedRows * selectedRows).fill(null));
+  }
+
   return (
     <div className="board">
-      <p className={`status ${status.startsWith("Game") || status === winner ? "win" : "tie"}`}>{status}</p>
+      <Dropdown
+        items={boardSizes}
+        handleSelect={(e) => handleReset(e)}
+      />
+      <p className={`status ${winnerClassName}`}>{status}</p>
       {allGrids.map((grid, i) => {
         return (
           <Grid
             key={i}
             index={i}
-            rows={props.rows}
+            rows={rows}
             value={grid}
             handleGridClick={handleGridClick}
           />
         )
       })
       }
+      <Button handleClick={handleReset}>Reset</Button>
     </div>
   );
 }
